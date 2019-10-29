20b660ac65dab4e24eb0860ba4b6f147b547e5f2
paulemfag <boiteprofagot@gmail.com>
2019-10-29 09:17:23 +0100

    Exo4

---
+++ assets/js/monscript.js
@@ -0,0 +1,15 @@
+var password = document.getElementById('password'); //onclick = function() {onclick()};
+var confirmPassword = document.getElementById('confirmPassword');// onclick = function() {onclick()};
+
+// function onclick(password = confirmPassword)
+
+button.onclick = function onclick();
+
+function onclick() {
+if (password==confirmPassword){
+  document.getElementById("password").backgroundColor = "green";
+}
+alert ( password + confirmPassword )
+  // document.getElementById("password").backgroundColor = "green";
+  // document.getElementById("confirmPassword").backgroundColor = "green";
+}
